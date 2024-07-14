d7abb2bf179465a88142ffbe4bcba7c8241089f9
Skylark <skylark@lambdaspire.com>
2024-07-14 13:20:28 +1000

    Outdated comment

--- Sources/LambdaspireSwiftDDD/ExportedMacros.swift
+++ Sources/LambdaspireSwiftDDD/ExportedMacros.swift
@@ -1,5 +1,4 @@
 
 @attached(member, names: named(events))
 @attached(extension, conformances: HasDomainEvents)
-// TODO: There's a warning here that doesn't seem to be correct. All the tests pass and usage seems fine here and in dependent projects.
 public macro DomainEntity() = #externalMacro(module: "LambdaspireSwiftDDDMacros", type: "DomainEntityMacro")
